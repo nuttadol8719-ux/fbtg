b84aefcada2aaa217e4414a2efac0910541c16e6
pondxd <nuttadol8719@gmail.com>
2026-02-01 00:08:21 +0700

    Main.lua

--- main.lua
+++ main.lua
@@ -7,7 +7,7 @@
 local Rayfield = loadstring(game:HttpGet("https://sirius.menu/rayfield"))()
 
 local Window = Rayfield:CreateWindow({
-    Name = "fruits battleground update1.30",
+    Name = "fruits battleground update1.35(pvp🔥)",
     LoadingTitle = "update",
     LoadingSubtitle = "by pond",
     ConfigurationSaving = {
